17d7f1d09f815029d20eb285df2b664f78330c26
artem723 <artem723@gmail.com>
2023-03-10 22:19:22 +0300

    add functions

--- internal/repository/balance.go
+++ internal/repository/balance.go
@@ -1,6 +1,10 @@
 package repository
 
-import "github.com/jmoiron/sqlx"
+import (
+	"bonus/internal/model"
+	"context"
+	"github.com/jmoiron/sqlx"
+)
 
 type balanceRepository struct {
 	db *sqlx.DB
@@ -8,4 +12,11 @@ type balanceRepository struct {
 
 func NewBalanceRepository(db *sqlx.DB) *balanceRepository {
 	return &balanceRepository{db}
+}
+
+func (b *balanceRepository) Create(ctx context.Context, withdrawal *model.Balance) error {
+	return nil
+}
+func (b *balanceRepository) GetByUserID(ctx context.Context, userID uint64) ([]*model.Balance, error) {
+	return nil, nil
 }
--- internal/service/repo.go
+++ internal/service/repo.go
@@ -6,12 +6,12 @@ import (
 )
 
 type OrderRepository interface {
-	Create(ctx context.Context, order model.Order) error
-	GetByUserID(ctx context.Context, userID uint64) ([]model.Order, error)
-	GetByNumber(ctx context.Context, number string) (model.Order, error)
+	Create(ctx context.Context, order *model.Order) error
+	GetByUserID(ctx context.Context, userID uint64) ([]*model.Order, error)
+	GetByNumber(ctx context.Context, number string) (*model.Order, error)
 }
 
 type BalanceRepository interface {
-	Create(ctx context.Context, withdrawal model.Balance) error
-	GetByUserID(ctx context.Context, userID uint64) ([]model.Balance, error)
+	Create(ctx context.Context, withdrawal *model.Balance) error
+	GetByUserID(ctx context.Context, userID uint64) ([]*model.Balance, error)
 }
