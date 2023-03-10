393fcf09da8617c2045e2e22d789e92f58a33aa7
artem723 <artem723@gmail.com>
2023-03-10 22:14:41 +0300

    change sql up

--- migrations/01_init_up.sql
+++ migrations/01_init_up.sql
@@ -1,29 +1,29 @@
-CREATE TABLE IF NOT EXISTS user
+CREATE TABLE IF NOT EXISTS users
 (
     id            bigserial primary key,
-    login         text   not null,
-    password_hash text   not null,
+    login         varchar(255)   not null,
+    password_hash varchar(255)   not null,
     CONSTRAINT    login_unique UNIQUE (login)
 );
 
-CREATE TABLE IF NOT EXISTS order
+CREATE TABLE IF NOT EXISTS orders
 (
     id          bigserial primary key,
     user_id     bigint         not null,
     number      varchar(255)   not null,
     status      varchar(10)    not null,
-    accrual     numeric(12, 2) not null default 0,
+    accrual     numeric(12, 2),
     uploaded_at Timestamp      not null,
     CONSTRAINT  number_unique UNIQUE (number)
 );
 
-CREATE TABLE IF NOT EXISTS balance
+CREATE TABLE IF NOT EXISTS balances
 (
     id          bigserial primary key,
     user_id     bigint         not null,
     ordernum    varchar(255)   not null,
-    debit       numeric(12, 2) not null,
-    credit      numeric(12, 2) not null,
+    debit       numeric(12, 2),
+    credit      numeric(12, 2),
     created_at  Timestamp      not null,
     CONSTRAINT  ordernum_unique UNIQUE (ordernum)
 );
